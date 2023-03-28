4031069c9b71fdf51c02f7f1d78d4b5b6c493f2c
anedaaee <anedaaee@gmail.com>
2023-03-29 03:27:12 +0330

    in this commit we create method for admin to can see server mmemory usage and cpu usage

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ const passport = require('passport');
 const userAuthRouter = require('./routers/users/auth/router.user.atuh');
 const stockRouter = require('./routers/stock/stock.router');
 const stockInProfileRouter = require('./routers/users/stockInProfile/router.user.stockInProfile');
+const serverUsageRouter = require('./routers/admin/serverStatus/router.admin.serverStatus');
 
 require('dotenv').config();
 require('./config/passport')(passport);
@@ -23,6 +24,7 @@ server.use(cors());
 server.use('/api/users/auth',userAuthRouter);
 server.use('/api/users/stockInProfile' ,stockInProfileRouter);
 server.use('/api/stock',stockRouter);
+server.use('/api/admin/serverStatus',serverUsageRouter);
 
 server.listen(process.env.PORT || 7170 , ()=>{
     console.log(`server is running on port ${process.env.PORT || 7170}`);
